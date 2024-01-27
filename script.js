51b1bf6eb53b31522fdb798f2f36bcfcb40074f6
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-01-27 15:53:24 -0500

    Added modulo function to calculator

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
-const mulitply = (a, b) => a * b;
+const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const power = (a, b) => a ** b;
+const modulo = (a, b) => {
+    if (b === 0) {
+        console.log('ERROR');
+        return NaN;
+    }
+    return a % b;
+}
 
 let operator, first, second;
 
 function operate(operator, first, second) {
     switch (operator) {
-        case 'add':
-            add(first, second);
-        case 'subtract':
-            subtract(first, second);
-        case 'multiply':
-            mulitply(first, second);
-        case 'divide':
-            divide(first, second);
+        case '+':
+            return add(first, second);
+        case '-':
+            return subtract(first, second);
+        case '*':
+            return multiply(first, second);
+        case '/':
+            return divide(first, second);
+        case '^':
+            return power(first, second);
+        case '%':
+            return modulo(first, second);
         default:
             console.log('ERROR');
+            return NaN;
     }
 }
