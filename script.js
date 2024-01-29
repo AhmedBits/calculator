c49ccf46cba25f4b3a72a97f7b6dbd69b7dd6e5f
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-01-29 18:35:34 -0500

    Fixed various bugs and variable names

--- script.js
+++ script.js
@@ -6,44 +6,55 @@ const ac = document.querySelector('#ac');
 
 const enterNumbers = (e) => display.innerHTML += e.target.id;
 const obtainDisplayValue = () => display.innerHTML;
-const clear = () => {
+const clearAll = () => {
     display.innerHTML = '';
     first = undefined;
     second = undefined;
     oldOperator = undefined;
     newOperator = undefined;
-    reset = false;
+    resetDisplay = false;
+    pressedEquals = false;
 };
 
-clear();
+clearAll();
 
 numbers.forEach(num => num.addEventListener('click', (e) => {
-    if (reset) {
+    if (resetDisplay) {
         display.innerHTML = '';
         enterNumbers(e);
-        reset = false;
+        resetDisplay = false;
+    } else if (pressedEquals) {
+        clearAll();
+        enterNumbers(e);
     } else {
         enterNumbers(e);
     }
 }));
 
 operators.forEach(operator => operator.addEventListener('click', (e) => {
+    // If this is a new compute, the oldOperator is the clicked event
     if (typeof oldOperator == 'undefined') {
         oldOperator = e.target.id;
     }
     newOperator = e.target.id;
 
-    if (typeof first == 'undefined') {
+    // If this is a new compute, store the current display value 
+    // as the first variable in a future operation
+    if (typeof first == 'undefined' || pressedEquals) {
         first = obtainDisplayValue();
-        reset = true;
+        resetDisplay = true;
+        pressedEquals = false;
+        oldOperator = e.target.id;
+    } else { 
+        // If this is a continued compute, the current calculation result 
+        // will be stored as the first variable in a future operation
 
-    } else {
         second = obtainDisplayValue();
         display.innerHTML = operate(oldOperator, first, second);
         first = display.innerHTML;
         second = undefined;
         oldOperator = newOperator;
-        reset = true;
+        resetDisplay = true;
     }
 }));
 
@@ -51,17 +62,24 @@ equals.addEventListener('click', () => {
     second = obtainDisplayValue();
     display.innerHTML = operate(newOperator, first, second);
     second = undefined;
+    pressedEquals = true;
 });
 
-ac.addEventListener('click', clear);
+ac.addEventListener('click', clearAll);
 
 const add = (a, b) => Number(a) + Number(b);
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => { 
+    if (b == 0) {
+        console.log('ERROR');
+        return NaN;
+    }
+    return a / b;
+}
 const power = (a, b) => a ** b;
 const modulo = (a, b) => {
-    if (b === 0) {
+    if (b == 0) {
         console.log('ERROR');
         return NaN;
     }
