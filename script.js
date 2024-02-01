70b90eda34ec8b52006197d19c8e26b69bc21684
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-01-31 19:02:50 -0500

    Formatted JS with prettier

--- script.js
+++ script.js
@@ -1,106 +1,106 @@
-const display = document.querySelector('#display');
-const numbers = document.querySelectorAll('.num');
-const operators = document.querySelectorAll('.operator');
-const equals = document.querySelector('#equals');
-const ac = document.querySelector('#ac');
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector("#equals");
+const ac = document.querySelector("#ac");
 
-const enterNumbers = (e) => display.innerHTML += e.target.id;
+const enterNumbers = (e) => (display.innerHTML += e.target.id);
 const obtainDisplayValue = () => display.innerHTML;
 const clearAll = () => {
-    display.innerHTML = '';
-    first = undefined;
-    second = undefined;
-    oldOperator = undefined;
-    newOperator = undefined;
-    resetDisplay = false;
-    pressedEquals = false;
-    pressedOperator = false;
+  display.innerHTML = "";
+  first = undefined;
+  second = undefined;
+  oldOperator = undefined;
+  newOperator = undefined;
+  resetDisplay = false;
+  pressedEquals = false;
+  pressedOperator = false;
 };
 
 clearAll();
 
-numbers.forEach(num => num.addEventListener('click', handleNumberClick));
-operators.forEach(operator => operator.addEventListener('click', handleOperatorClick));
-equals.addEventListener('click', handleEqualClick);
-ac.addEventListener('click', clearAll);
+numbers.forEach((num) => num.addEventListener("click", handleNumberClick));
+operators.forEach((operator) => operator.addEventListener("click", handleOperatorClick));
+equals.addEventListener("click", handleEqualClick);
+ac.addEventListener("click", clearAll);
 
 const add = (a, b) => Number(a) + Number(b);
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => { 
-    if (b == 0) {
-        return undefined;
-    }
-    return a / b;
-}
+const divide = (a, b) => {
+  if (b == 0) {
+    return undefined;
+  }
+  return a / b;
+};
 const power = (a, b) => a ** b;
 const modulo = (a, b) => {
-    if (b == 0) {
-        return undefined;
-    }
-    return a % b;
-}
+  if (b == 0) {
+    return undefined;
+  }
+  return a % b;
+};
 
 function operate(operator, first, second) {
-    switch (operator) {
-        case '+':
-            return add(first, second);
-        case '-':
-            return subtract(first, second);
-        case '*':
-            return multiply(first, second);
-        case '/':
-            return divide(first, second);
-        case '^':
-            return power(first, second);
-        case '%':
-            return modulo(first, second);
-        default:
-            return NaN;
-    }
+  switch (operator) {
+    case "+":
+      return add(first, second);
+    case "-":
+      return subtract(first, second);
+    case "*":
+      return multiply(first, second);
+    case "/":
+      return divide(first, second);
+    case "^":
+      return power(first, second);
+    case "%":
+      return modulo(first, second);
+    default:
+      return NaN;
+  }
 }
 
 function handleNumberClick(e) {
-    if (resetDisplay) {
-        display.innerHTML = '';
-        resetDisplay = false;
-    } else if (pressedEquals) {
-        clearAll();
-    }
-    enterNumbers(e);
-    pressedOperator = false;
+  if (resetDisplay) {
+    display.innerHTML = "";
+    resetDisplay = false;
+  } else if (pressedEquals) {
+    clearAll();
+  }
+  enterNumbers(e);
+  pressedOperator = false;
 }
 
 function handleOperatorClick(e) {
-    newOperator = e.target.id;
+  newOperator = e.target.id;
 
-    if (pressedOperator) {
-        oldOperator = newOperator;
-        return;
-    }
+  if (pressedOperator) {
+    oldOperator = newOperator;
+    return;
+  }
 
-    if (typeof oldOperator == 'undefined' || pressedEquals) {
-        first = obtainDisplayValue();
-        oldOperator = e.target.id;
-        resetDisplay = true;
-        pressedEquals = false;
-    } else {
-        second = obtainDisplayValue();
-        display.innerHTML = operate(oldOperator, first, second);
-        first = display.innerHTML;
-        second = undefined;
-        oldOperator = newOperator;
-        resetDisplay = true;
-    }
-    pressedOperator = true;
+  if (typeof oldOperator == "undefined" || pressedEquals) {
+    first = obtainDisplayValue();
+    oldOperator = e.target.id;
+    resetDisplay = true;
+    pressedEquals = false;
+  } else {
+    second = obtainDisplayValue();
+    display.innerHTML = operate(oldOperator, first, second);
+    first = display.innerHTML;
+    oldOperator = newOperator;
+    second = undefined;
+    resetDisplay = true;
+  }
+  pressedOperator = true;
 }
 
 function handleEqualClick() {
-    if (pressedEquals || pressedOperator || typeof first == 'undefined') {
-        return;
-    }
-    second = obtainDisplayValue();
-    display.innerHTML = operate(newOperator, first, second);
-    second = undefined;
-    pressedEquals = true;
+  if (pressedEquals || pressedOperator || typeof first == "undefined") {
+    return;
+  }
+  second = obtainDisplayValue();
+  display.innerHTML = operate(newOperator, first, second);
+  second = undefined;
+  pressedEquals = true;
 }
